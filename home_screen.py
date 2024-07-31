73867101f258d0f2c7a158d277918c6a7449cccc
Blacktune <Blacktune2020@gmail.com>
2024-08-01 11:33:00 +1200

    This is V1 Of my game - Fishing Mystery

---
+++ home_screen.py
@@ -0,0 +1,94 @@
+import pygame
+from pygame.locals import *
+import random
+from lvl1_screen import (lvl1_screen)
+
+pygame.init()
+clock = pygame.time.Clock()
+fps = 60
+
+# Set screen width and height
+screen_width = 936
+screen_height = 864
+
+
+#set play button size
+pb_width = 500
+pb_height = 500
+
+#set tutorial button size
+tb_width = 500
+tb_height = 500
+
+#set tutorial button size
+sb_width = 500
+sb_height = 500
+
+# Create the screen with titels
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("The Fishing Mystery")
+
+# Define the font for the game
+font = pygame.font.SysFont('Baus 93', 60)
+
+
+pygame.mixer.init()  # initialize the mixer
+pygame.mixer.music.stop()  # stop the previous song muisic
+
+# Load and play the song
+pygame.mixer.music.load('sound.mp3/home_screen_sound.mp3')
+pygame.mixer.music.play(-1)
+
+# Create our background image for our game
+bg = pygame.image.load('img/th.png')
+bg = pygame.transform.scale(bg, (screen_width, screen_height))  # resizes the image to fill the screen
+
+#Create the play button
+pb = pygame.image.load('img/pixilart-drawing.png')
+pb = pygame.transform.scale(pb, (pb_width, pb_height))
+
+#create the tutorial button
+tb = pygame.image.load('img/tutorial_button.png')
+tb = pygame.transform.scale(tb, (tb_width, tb_height))
+
+#create the settings button
+sb = pygame.image.load('img/settings_button.png')
+sb = pygame.transform.scale(sb,(sb_width, sb_height))
+
+
+#Set playbutton location
+pb_x = (screen_width - screen_height) / 2
+pb_y = 0
+
+#set tutorial button location
+tb_x = (screen_width - screen_height) / 1 + 100
+tb_y = 0
+
+#set settings button loaction
+sb_x = (screen_width - screen_height) / 1 - 73
+sb_y = 300
+
+# Create a screen that our game will be running on
+run = True
+while run:
+    clock.tick(fps)
+    screen.blit(bg, (0, 0))
+    screen.blit(pb, (pb_y,pb_x))
+    screen.blit(tb, (tb_y, tb_x))
+    screen.blit(sb, (sb_x,sb_y))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # Get key states
+    keys = pygame.key.get_pressed()
+
+    #go to lvl1 page
+    if keys[K_p]:
+        lvl1_screen()  # switch to the new screen
+        run = False  # end the current screen
+
+    pygame.display.update()
+pygame.quit()
+
---
+++ lvl1_screen.py
@@ -0,0 +1,95 @@
+import pygame
+from pygame.locals import *
+import random
+
+def lvl1_screen():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    fps = 60
+
+    # Set screen width and height
+    screen_width = 936
+    screen_height = 864
+
+    # Set character height and width
+    ch_height = 50
+    ch_width = 50
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("The Fishing Mystery")
+
+    pygame.mixer.init()  # initialize the mixer
+    pygame.mixer.music.stop()  # stop the previous song muisic
+
+    # Load and play the song
+    pygame.mixer.music.load('sound.mp3/lvl1_song.mp3')
+    pygame.mixer.music.play(-1)
+
+    # Define the font for the game
+    font = pygame.font.SysFont('Baus 93', 60)
+
+    # Create our background image for our game
+    bg = pygame.image.load('img/th.png')
+    bg = pygame.transform.scale(bg, (screen_width, screen_height))  # resizes the image to fill the screen
+
+    # Create our playable character
+    ch = pygame.image.load('img/Fishing_Character-removebg-preview.png')
+    ch = pygame.transform.scale(ch, (ch_width, ch_height))
+
+    # Set initial character position
+    ch_x = screen_width // 2 - ch_width // 2
+    ch_y = screen_height // 2 - ch_height // 2 + 135
+    # Set the speed of the character
+    speed = 5
+    # Define the boundaries for obstacles
+    house_rect = pygame.Rect(370, 350, 200, 200) # x, y, width, height
+    left_lake_rect = pygame. Rect (0,400,340,500) #x, y, width, height
+    horizon_y = 500 # y-coordinate of the horizon line
+
+    # Create a screen that our game will be running on
+    run = True
+    while run:
+        clock.tick(fps)
+        screen.blit(bg, (0, 0))
+        screen.blit(ch, (ch_x, ch_y))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # Get key states
+        keys = pygame.key.get_pressed()
+
+        # Move left
+        if keys[K_LEFT]:
+            new_x = ch_x - speed
+            if new_x >= 0 and not (house_rect.collidepoint(new_x, ch_y) or
+                                   left_lake_rect.collidepoint(new_x, ch_y)):
+                ch_x = new_x
+
+        # Move right
+        if keys[K_RIGHT]:
+            new_x = ch_x + speed
+            if new_x <= screen_width - ch_width and not (house_rect.collidepoint(new_x + ch_width, ch_y) or
+                                                         left_lake_rect.collidepoint(new_x + ch_width, ch_y)):
+                ch_x = new_x
+
+        # Move up
+        if keys[K_UP]:
+            new_y = ch_y - speed
+            if new_y >= 0 and new_y >= horizon_y and not (house_rect.collidepoint(ch_x, new_y) or
+                                                          left_lake_rect.collidepoint(ch_x, new_y)):
+                ch_y = new_y
+
+        # Move down
+        if keys[K_DOWN]:
+            new_y = ch_y + speed
+            if new_y <= screen_height - ch_height and not (house_rect.collidepoint(ch_x, new_y + ch_height) or
+                                                           left_lake_rect.collidepoint(ch_x, new_y + ch_height)):
+                ch_y = new_y
+
+        pygame.display.update()
+
+    pygame.quit()
+pass
